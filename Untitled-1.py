ab88f184395b6bb93d0f4b494a568639bf0e9cc6
MarcosDEV7 <contato.marcosdev@gmail.com>
2021-08-28 16:07:01 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,36 @@
-print('Olá Mundo')
+import time
+
+#============Testanto time============
+print('Teste de Delay')
+time.sleep(5)
+print('Ok, teste funcionando')
+time.sleep(2)
+#=====================================
+
+#============Solicitando entrada de dados============
+print('Digite um numero')
+n1 = int(input())
+print('Digite outro numero')
+n2 = int(input())
+#====================================================
+
+
+#============Somda dos dados============
+print('Deseja Somar os numeros')
+res = input()
+    #Função Aguarde...
+def mensagem(x):
+    print(x)
+    time.sleep(2)
+mensagem('Aguarde...')
+    #Verifica opção digitada
+if res == ('Sim'):
+    mensagem
+    print (n1 + n2)
+elif res == ('Não'):
+    mensagem
+    print('Ok, Obrigado por utilizar o programa')
+else:
+    mensagem
+    print('Digite apenas Sim ou Não')
+#========================================
